7c2bab6fd4e26291a5ec266c3a9d99a845e2bc3a
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-09-30 14:12:44 -0500

    first commit to workout app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h1 className="App-title">Tom's Workout App</h1>
         </header>
         <br/>
       </div>
