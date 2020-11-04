ebf1dea17939441f79955d825606003d374b6441
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-04 10:08:48 -0600

    material ui imported in legs

--- src/components/Legs/Legs.js
+++ src/components/Legs/Legs.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 //import connect for use of redux
 import {connect} from 'react-redux';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 class Legs extends Component {
 
