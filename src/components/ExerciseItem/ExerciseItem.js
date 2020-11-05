2ad85d94502ad3e2a23ef63f2535e629cd2f2032
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-05 10:34:02 -0600

    update exercise item

--- src/components/ExerciseItem/ExerciseItem.js
+++ src/components/ExerciseItem/ExerciseItem.js
@@ -10,22 +10,7 @@ import TableRow from '@material-ui/core/TableRow';
 
 class ExerciseItem extends Component {
 
-handleDelete = (id)=>(event)=>{
-console.log('delete clicked, id is', id);
-this.props.dispatch({type: 'DELETE_EVENT', payload: id});
-}//end handleDelete
 
-goEdit = (id) => (event)=> {
-  console.log('edit clicked, id is', id)
-  this.props.dispatch({type: 'GET_DETAILS', payload: id}) 
-  this.props.history.push('/edit/' + id) 
-}
-
-goDetails = (id) => (event)=> {
-  console.log('details clicked, id is', id)
-  this.props.dispatch({type: 'GET_DETAILS', payload: id}) 
-  this.props.history.push('/details/') 
-}
   render() {
     return (
       <TableRow>
