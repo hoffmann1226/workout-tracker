6b2484c7174894b59561b084b72ae69de139e6ed
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-06 09:53:01 -0600

    legs table data entered

--- data.sql
+++ data.sql
@@ -72,10 +72,38 @@ VALUES
   ('Crouching Cohen Curls', 0, 0);
    ('Lying-Down Triceps Extensions', 0, 0),
   ('Water Break', 0, 0),
-  ('In and Out Straight Arm Shoulder Flys', 0, 0);
+  ('In and Out Straight Arm Shoulder Flys', 0, 0)
    ('Congdon Curls', 0, 0),
-  ('Side Tri-Rises', 0, 0);
-  ('In and Out Straight Arm Shoulder Flys', 0, 0);
+  ('Side Tri-Rises', 0, 0)
+  ('In and Out Straight Arm Shoulder Flys', 0, 0)
    ('Congdon Curls', 0, 0),
   ('Side Tri-Rises', 0, 0);
 
+INSERT INTO "legs" ("exercise", "reps", "weight")
+VALUES 
+  ('Balanced Lunges', 0, 0),
+  ('Calf-Raise Squats', 0, 0),
+  ('Reverse-Grip Chin-Ups', 0, 0),
+  ('Super Skaters', 0, 0),
+  ('Wall Squats', 0, 0),
+  ('Wide Front Pull-Ups', 0, 0),
+  ('Step Back Lunge', 0, 0),
+  ('Alternating Side Lunge', 0, 0),
+  ('Cloded Grip Overhead Pull-Ups', 0, 0),
+  ('Singe Leg Wall Squat', 0, 0),
+  ('Switch Grip Pull-Ups', 0, 0),
+  ('Water Break', 0, 0),
+  ('Three-Way Lunge', 0, 0),
+  ('Sneaky Lunge', 0, 0),
+  ('Reverse Grip Chin-Ups', 0, 0),
+  ('Chair Salutations', 0, 0),
+  ('Toe Roll Iso Lunge', 0, 0);
+   ('Wide Front Pull-Ups', 0, 0),
+  ('Groucho Walk', 0, 0),
+  ('Calf Raises Toes Out', 0, 0);
+   ('Calf Raises Parallel', 0, 0),
+  ('Calf Raises In', 0, 0),
+  ('Closed Grip Overhand Pull-Ups', 0, 0);
+   ('80/20 Siebers-Speed Squats', 0, 0),
+  ('Switch Grip Pull Ups', 0, 0);
+  
