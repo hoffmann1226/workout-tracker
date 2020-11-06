b808aa1be6f1afd15b36c55b59b28cd06b92fc3a
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-06 09:43:42 -0600

    shoulder table data entry

--- data.sql
+++ data.sql
@@ -42,3 +42,40 @@ VALUES
   ('Dive-Bomber Push-Ups', 0, 0),
   ('Back Flys', 0, 0);
 
+INSERT INTO "shoulders" ("exercise", "reps", "weight")
+VALUES 
+  ('Alternating Shoulder Presses', 0, 0),
+  ('In and Out Bicep Curls', 0, 0),
+  ('Two Arm Tricep Kickback', 0, 0),
+  ('Alternating Shoulder Presses', 0, 0),
+  ('In and Out Bicep Curls', 0, 0),
+  ('Two Arm Tricep Kickback', 0, 0),
+  ('Water Break', 0, 0),
+  ('Deep Swimmer Presses', 0, 0),
+  ('Full Supination Concentration Curls', 0, 0),
+  ('Chair Dips', 0, 0),
+  ('Deep Swimmer Presses', 0, 0),
+  ('Full Supination Concentration Curls', 0, 0),
+  ('Chair Dips', 0, 0),
+  ('Water Break', 0, 0),
+  ('Upright Rows', 0, 0),
+  ('Static Arm Curls', 0, 0),
+  ('Flip Grip Tricep Kickbacks', 0, 0);
+   ('Upright Rows', 0, 0),
+  ('Static Arm Curls', 0, 0),
+  ('Flip Grip Tricep Kickbacks', 0, 0);
+   ('Water Break', 0, 0),
+  ('Seater Two-Angle Shoulder Flys', 0, 0),
+  ('Crouching Cohen Curls', 0, 0);
+   ('Lying-Down Triceps Extensions', 0, 0),
+  ('Seater Two-Angle Shoulder Flys', 0, 0),
+  ('Crouching Cohen Curls', 0, 0);
+   ('Lying-Down Triceps Extensions', 0, 0),
+  ('Water Break', 0, 0),
+  ('In and Out Straight Arm Shoulder Flys', 0, 0);
+   ('Congdon Curls', 0, 0),
+  ('Side Tri-Rises', 0, 0);
+  ('In and Out Straight Arm Shoulder Flys', 0, 0);
+   ('Congdon Curls', 0, 0),
+  ('Side Tri-Rises', 0, 0);
+
