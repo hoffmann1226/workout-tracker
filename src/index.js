7aa326103c7bdd3e9aba6f5a7b331b0f4638ea10
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-09 07:44:55 -0600

    chest saga and reducer created

--- src/index.js
+++ src/index.js
@@ -6,10 +6,40 @@ import registerServiceWorker from './registerServiceWorker';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'; 
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import {takeEvery, put} from 'redux-saga/effects';
+import axios from 'axios';
+
+function* rootSaga(){
+  yield takeEvery('GET_CHEST', getChest)
+}
+
+function* getChest (){
+  try{
+    let response = yield axios.get(`/chest`)
+    console.log('in get chest saga', response.data)
+    yield put({
+      type: 'SET_CHEST',
+      payload: response.data
+    })
+  } catch(error){
+    console.log('problem with get chest saga', error)
+  }
+}
+
+const chestReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_CHEST':
+      return action.payload;
+    default:
+      return state;
+  }
+}
 
 //store instance and combine reducers
 const storeInstance = createStore(
     combineReducers({
+      chestReducer
     }),
     applyMiddleware(logger)
   );
