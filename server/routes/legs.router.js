bf3ac5e59804a9d3b60476726fa7f13f2398adc4
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-12 07:00:45 -0600

    legs router created

---
+++ server/routes/legs.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log('in shoulders router')
+    const queryText = 'SELECT * FROM legs';
+    pool.query(queryText)
+      .then((result) => { res.send(result.rows); })
+      .catch((err) => {
+        console.log('Error getting chest', err);
+        res.sendStatus(500);
+      });
+  });
+
+ 
+
+  module.exports = router;
