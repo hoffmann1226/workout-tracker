391734ea2c341351fe614ac135cf8a61eb62a017
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-20 07:31:29 -0600

    cst router created

---
+++ server/routes/cst.router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log ('in cst router')
+    const queryText = 'SELECT * FROM chest';
+    pool.query (queryText)
+        .then((result) => { res.send(result.rows); })
+        .catch((err) => {
+            console.log('Error getting cst', err);
+            res.sendStatus(500);
+        });
+});
