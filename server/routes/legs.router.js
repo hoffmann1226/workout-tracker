98bf6697d8e60d536ff69e5c8ef5db038e857dfb
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-11-24 08:22:43 -0600

    shoulders commented

--- server/routes/legs.router.js
+++ server/routes/legs.router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //get route for shoulders data
 router.get('/', (req, res) => {
-    console.log('in shoulders router')
+    console.log('in legs router')
     const queryText = 'SELECT * FROM legs';
     pool.query(queryText)
       .then((result) => { res.send(result.rows); })
